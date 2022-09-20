0954486c447fb44418614f35837d16168959af64
sujit9511 <sujitgaikwad9511@gmail.com>
2022-09-21 01:42:12 +0530

    firebasejs line 1

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,14 @@
-import firebase from 'firebase';
+import firebase from "firebase/app";
 
-const config={
-        apiKey: "AIzaSyBFZc39yMDDsA9LVYsNQuDBdeBaq6Z8SdE",
-        authDomain: "trackingapp-bfad3.firebaseapp.com",
-        projectId: "trackingapp-bfad3",
-        storageBucket: "trackingapp-bfad3.appspot.com",
-        messagingSenderId: "64645875804",
-        appId: "1:64645875804:web:9d6226845b7880fca77e99",
-        measurementId: "G-7F1Z3HC7B2"
-     
-
-}
+const config = {
+  apiKey: "AIzaSyBFZc39yMDDsA9LVYsNQuDBdeBaq6Z8SdE",
+  authDomain: "trackingapp-bfad3.firebaseapp.com",
+  projectId: "trackingapp-bfad3",
+  storageBucket: "trackingapp-bfad3.appspot.com",
+  messagingSenderId: "64645875804",
+  appId: "1:64645875804:web:9d6226845b7880fca77e99",
+  measurementId: "G-7F1Z3HC7B2",
+};
 
 firebase.initializeApp(config);
-export default firebase
-
+export default firebase;
